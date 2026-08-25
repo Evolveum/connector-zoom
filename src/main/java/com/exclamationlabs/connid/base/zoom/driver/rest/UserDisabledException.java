80b8ed74610bee24a0945bb97e20a958ec48ba14
Richard Frovarp <richard.frovarp@ndsu.edu>
2026-08-25 11:29:36 -0500

    Allow for the updating of disabled users.
    Or to just ignore the changes / error coming from the system.

--- src/main/java/com/exclamationlabs/connid/base/zoom/driver/rest/UserDisabledException.java
+++ src/main/java/com/exclamationlabs/connid/base/zoom/driver/rest/UserDisabledException.java
@@ -1,31 +1,25 @@
 /*
-    Copyright 2020 Exclamation Labs
-
+    Copyright 2026 Evolveum
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
     You may obtain a copy of the License at
-
         http://www.apache.org/licenses/LICENSE-2.0
-
     Unless required by applicable law or agreed to in writing, software
     distributed under the License is distributed on an "AS IS" BASIS,
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     See the License for the specific language governing permissions and
     limitations under the License.
 */
+package com.exclamationlabs.connid.base.zoom.driver.rest;
 
-package com.exclamationlabs.connid.base.zoom.model.response.fault;
+import org.identityconnectors.framework.common.exceptions.ConnectorException;
 
-public interface ErrorResponseCode {
-  int PAID_SUBSCRIPTION_REQUIRED = 200;
-  int VALIDATION_FAILED = 300;
-
-  int USER_NOT_FOUND = 1001;
-  int USER_ALREADY_EXISTS = 1005;
-  int USER_DISABLED = 1053;
-  int REQUIRES_MANAGED_DOMAIN = 1116;
-
-  int GROUP_NOT_FOUND = 4130;
-  int GROUP_NAME_ALREADY_EXISTS = 4132;
-  int TOKEN_EXPIRED = 124;
+/**
+ * Fired when a user can't be updated due to being disabled.
+ *
+ */
+public class UserDisabledException extends ConnectorException {
+  public UserDisabledException(String message) {
+        super(message);
+  }
 }
